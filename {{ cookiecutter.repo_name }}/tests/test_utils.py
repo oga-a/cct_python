ad18598a7c9c4fec318c48c3d05d9c06aaf1a60c
Oga Akihiro <5.11.moc.liamg+other@gmail.com>
2022-05-06 02:24:14 +0900

    MODIFY

--- {{ cookiecutter.repo_name }}/tests/test_utils.py
+++ {{ cookiecutter.repo_name }}/tests/test_utils.py
@@ -1,14 +1,14 @@
 import pytest
-from {{ cookiecutter.python_package }} import __version__
+from {{ cookiecutter.python_package }} import utils
 
 
 @pytest.mark.parametrize("x,sum", [(1, 2), (2, 3), (123, 124)])
 def test_add_one(x: int, sum: int):
-    x = {{ cookiecutter.python_package }}.utils.add_one(x)
+    x = utils.add_one(x)
     assert x == sum
 
 
 @pytest.mark.parametrize("x,sum", [(1, 3), (2, 4), (123, 125)])
 def test_add_two(x: int, sum: int):
-    x = {{ cookiecutter.python_package }}.add_two(x)
+    x = utils.add_two(x)
     assert x == sum
